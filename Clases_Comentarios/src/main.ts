11e4aa3ea25428f9de5a53d0519b64008e5f68be
randymxd06 <randym0624@gmail.com>
2024-02-05 11:03:52 -0400

    feat: :sparkles: Agregue la seccion de clases y comentarios
    Esta seccion enseña e problema de la herencia, como manejar el principio de responsabilidad unica, como hacer comentarios de codigo entre otras cosas mas

---
+++ Clases_Comentarios/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import './clean-code/07-tarea';
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+`
+

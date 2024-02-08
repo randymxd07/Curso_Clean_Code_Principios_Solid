7467d128bab54ae1b661ebd37e0b815c319e71cd
randymxd06 <randym0624@gmail.com>
2024-02-08 08:57:20 -0400

    feat: :sparkles: Agregué la seccion de SOLID al repositorio
    Hice los ejemplos de los principios solid, aca se vieron los principios: SRP, OCP, LSP, ISP Y DIP

---
+++ SOLID/src/main.ts
@@ -0,0 +1,14 @@
+import './style.css'
+// import './clean-code/07-tarea';
+// import './solid/01-srp';
+// import './solid/02-open-close-a';
+// import './solid/03-liskov-a';
+import './solid/05-dependency-a';
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+`
+

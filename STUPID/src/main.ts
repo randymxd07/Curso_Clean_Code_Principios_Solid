ee37d7208c66f3e626bda6fd2432e93ed6b133f0
randymxd06 <randym0624@gmail.com>
2024-02-08 16:49:09 -0400

    feat: :sparkles: Volvi a agregar la carpeta de STUPID
    Tuve que borrar y volver a crear la carpeta de STUPID para seguir con la nomenclatura de las carpetas

---
+++ STUPID/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+// import './clean-code/07-tarea';
+import './code-smells/02-low-coupling';
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+  Hola mundo
+`
+
